b71291cc38c7c8d35e899c1f7b1cee0041d52fa8
elryi <nathaniel.edward.cook@gmail.com>
2023-12-08 16:12:44 -0500

    added imgs and some js

--- pointAndClickJS/pointAndClickJS.js
+++ pointAndClickJS/pointAndClickJS.js
@@ -1,4 +1,50 @@
+
+//This works for clicking from img to another by
+//changing the source and keeps it in the same screen
+
 function changeThis() {
-    let img = document.getElementById('gate');
-    img.src = "/Main Hall.jpg"
-}
+    document.getElementById("gate").src = "/pointAndClickImg/Main Hall.jpg";
+}    
+
+
+
+
+
+//Same thing as above + extra step?
+/*
+function gateToMainHall() {
+    let gate = document.getElementById('gate');
+    gate.src = "/pointAndClickImg/Main Hall.jpg";
+}    
+*/
+
+
+
+
+/* some ideas
+switch (stage) {
+    case 1:
+      //clicking left goes to this area
+      document.getElementById("this picture ID src")
+      break;
+    case 2:
+      //clicking the stairs leads to this area
+      document.getElementById("this picture ID src")
+      break;
+    case 3:
+     // clicking right leads to this area
+      document.getElementById("this picture ID src")
+      break;
+    default:
+      break;
+  }
+  */
+  
+  /* some more ideas...
+  function changeLevel(imageID, imageMap, newLevel) {
+    let stairsChoice = document.getElementById("upstairs-room");
+    let leftChoice = document.getElementById("dungeon");
+    let rightChoice = document.getElementById("potions-room")
+    if ()
+  }
+  */
